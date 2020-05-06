a3ab3d95a9d024df09113883a6fdc94a453b9d7c
AydarF <aydar_fayzullin@yahoo.com>
2020-05-06 14:18:41 -0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favorite episode</p>
+    </>
+  );
+}
+
+export default App;
